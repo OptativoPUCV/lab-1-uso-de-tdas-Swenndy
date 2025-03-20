f6f246f37e2b22d6393ba0c1b778f1467bc33a1f
Your Name <you@example.com>
2025-03-19 22:26:31 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -6,34 +6,37 @@
 #include "arraylist.h"
 #include "stack.h"
 
-//#include "exercises.h"
+// #include "exercises.h"
 
-//Funciones auxiliares que puedes utilizar para debuggear tus ejercicios
-//NO MODIFICAR
-void imprime_lista(List *L) {
+// Funciones auxiliares que puedes utilizar para debuggear tus ejercicios
+// NO MODIFICAR
+void imprime_lista(List *L)
+{
    int *dato;
-   dato = (int*)first(L);
+   dato = (int *)first(L);
    printf("[");
-   while(dato != NULL) {
+   while (dato != NULL)
+   {
       printf("%d ", *dato);
-      dato = (int*)next(L);
+      dato = (int *)next(L);
    }
    printf("]\n");
-
 }
 
-//Ojo que la pila se vacía al imprimir y se imprime en orden inverso
-//NO MODIFICAR
-void imprime_y_vacia_pila(Stack *P) {
+// Ojo que la pila se vacía al imprimir y se imprime en orden inverso
+// NO MODIFICAR
+void imprime_y_vacia_pila(Stack *P)
+{
    void *dato;
    printf("[");
-   while((dato = pop(P)) != NULL) {
-      printf("%d ", *(int*)dato);
+   while ((dato = pop(P)) != NULL)
+   {
+      printf("%d ", *(int *)dato);
    }
    printf("]\n");
 }
 
-/* 
+/*
 Ejercicio 1.
 Crea una Lista y agrega punteros a elementos del 1 al 10.
 Recuerda que la lista almacena punteros, por lo que
@@ -41,14 +44,15 @@ debes reservar memoria para cada elemento que agregues.
 Al finalizar retorna la lista creada.
 */
 
-List* crea_lista() {
-   List* L = create_list();
+List *crea_lista()
+{
+   List *L = create_list();
 
-   for (int i=1; i <= 10; i++)
+   for (int i = 1; i <= 10; i++)
    {
-      int *elemento = (int*)malloc(sizeof(int));
-      *(elemento)=i;
-      pushBack(L,elemento);
+      int *elemento = (int *)malloc(sizeof(int));
+      *(elemento) = i;
+      pushBack(L, elemento);
    }
 
    return L;
@@ -56,33 +60,44 @@ List* crea_lista() {
 
 /*
 Ejercicio 2.
-Crea una función que reciba una lista de enteros (int*) y 
+Crea una función que reciba una lista de enteros (int*) y
 retorne la suma de sus elementos.
 */
-int sumaLista(List *L) {
+int sumaLista(List *L)
+{
    int suma = 0;
    int *num = first(L);
-   while (num!=NULL)
+   while (num != NULL)
    {
       suma += (*num);
-      num=next(L);
+      num = next(L);
    }
-   
+
    return suma;
 }
 
 /*
 Ejercicio 3.
 Crea una función que reciba una lista de punteros a int (int*) y
-un entero elem. La función debe eliminar todos los elementos 
+un entero elem. La función debe eliminar todos los elementos
 de la lista que sean iguales a elem.
 Asume que popCurrent luego de eliminar un elemento se
 posiciona en el elemento anterior.
 */
 
-void eliminaElementos(List*L, int elem)
+void eliminaElementos(List *L, int elem)
 {
-
+   int *num = first(L);
+   while (num != NULL)
+   {
+      if (*num == elem)
+      {
+         popCurrent(L);
+         num = next(L);
+      }
+      else
+         num = next(L);
+   }
 }
 
 /*
@@ -92,17 +107,18 @@ El orden de ambas pilas se debe mantener.
 Puedes usar una pila auxiliar.
 */
 
-void copia_pila(Stack* P1, Stack* P2) {
+void copia_pila(Stack *P1, Stack *P2)
+{
 }
 
 /*
 Ejercicio 5.
-La función verifica si la cadena de entrada tiene sus 
+La función verifica si la cadena de entrada tiene sus
 paraéntesis balanceados. Retorna 1 si están balanceados,
 0 en caso contrario.
 */
 
-int parentesisBalanceados(char *cadena) {
+int parentesisBalanceados(char *cadena)
+{
    return 0;
 }
-
